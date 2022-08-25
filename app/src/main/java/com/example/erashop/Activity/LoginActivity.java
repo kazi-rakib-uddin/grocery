d04ddb8e6372e7aaa33a53e55a9944a59f823af9
Devgkarmkar <dev.gourabkarmakar@gmail.com>
2022-08-25 19:31:12 +0530

    gourab 25-08-22

--- app/src/main/java/com/example/erashop/Activity/LoginActivity.java
+++ app/src/main/java/com/example/erashop/Activity/LoginActivity.java
@@ -18,9 +18,9 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         getSupportActionBar().hide();
